a771822f43b617cc47d7604da061b5c5c1d149ad
qtcdrey <andreydelrosario8@gmail.com>
2024-11-15 20:27:48 +0800

    need to improve

--- body_mass_index.c
+++ body_mass_index.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    float bmi,height,weight;
+    printf("Weight(kg): ");
+    scanf("%f",&weight);
+    printf("Height(m): ");
+    scanf("%f",&height);
+    bmi = weight/(height*height);
+    printf("BMI = %.1f\n",bmi);
+    if (bmi >= 16.0 && bmi <= 18.5) {
+        puts("Underweight");
+    } else if (bmi >= 18.6 && bmi <= 25.0) {
+        puts("Normal");
+    } else  {
+        puts("Overweight");
+    }
+}
